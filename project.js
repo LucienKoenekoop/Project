c4bed1e07d6c06a95ab176c8e55f64e841525ee8
LucienKoenekoop <lucien.koenekoop@gmail.com>
2018-01-11 23:48:30 +0100

    prototype
    week 1

--- project.js
+++ project.js
@@ -1,4 +1,17 @@
 window.onload = function () {
+
+	var slider = document.getElementById("myRange");
+	var output = document.getElementById("demo");
+	output.innerHTML = slider.value;	
+	
+	slider.oninput = function() {
+		if (this.value == 2018) {
+			output.innerHTML = "Target";
+		}
+		else {
+			output.innerHTML = this.value;	
+		}
+	}
 	
 	queue()
 		.defer(d3.xml, "europe.svg")
@@ -9,5 +22,16 @@ window.onload = function () {
 		if (error) throw error;
 
 		document.body.appendChild(xml.documentElement);
+
+		var countries = ["#BE", "#NL", "#LU", "#FR", "#DE", "#IT", "#ES", "#PT",
+						 "#AT", "#CZ", "#PL", "#SE", "#IE", "#DK", "#BG", "#EE",
+						 "#GR", "#HR", "#CY", "#LV", "#LT", "#HU", "#MA", "#RO",
+						 "#SI", "#SK", "#FI", "#IS"];
+
+		for (i = 0; i <= countries.length - 1; i++) {
+			country = d3.select(countries[i])
+			.attr("stroke", "white")
+			.attr("fill", "rgba(0, 0, 153, 1)");
+		}
 	}
 }
